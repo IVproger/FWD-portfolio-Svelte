14a36cc9544003f717251ee6ded17ca93145e7ca
IVproger <108990576+IVproger@users.noreply.github.com>
2023-07-11 05:39:03 +0000

    Deploying to gh-pages from @ IVproger/FWD-portfolio-svelte@bb2606ba231ecc7d34ba52f67a3b4400b524fd12 🚀

---
+++ _app/immutable/chunks/stores.a15958f5.js
@@ -0,0 +1,1 @@
+import{d as e}from"./singletons.86e7046b.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
